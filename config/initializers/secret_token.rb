5cf10db755343bdc97c5a17123c6f47ac882845c
Marcelo Guilherme Jacobus Jr <marcelo.jacobus@gmail.com>
2012-09-27 15:26:02 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diego::Application.config.secret_token = 'f2334d03933b2b5d3ed82030331dcda5891c00e60ef80f8f30543b1416beb779873f4b2a5fb09b4a6c350aff3aad210ebb0eaad904c5c4292af0f45566cfba0e'
