76af27cd7d60bd832f76fa21f110cd58398c08f0
Marcelo Guilherme Jacobus Jr <marcelo.jacobus@gmail.com>
2012-10-02 14:36:29 -0300

    Adicionado seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Emanuel', :city => cities.first)
+# encoding: utf-8
+
+
+# Picture.delete_all
+# Product.delete_all
+# Service.delete_all
+# ProductType.delete_all
+
+@maquina = ProductType.create!(
+  :name => 'Máquina',
+  :slug => 'maquinas',
+  :sequence => 1
+)
+
+@peca = ProductType.create!(
+  :name => 'Peça',
+  :slug => 'pecas',
+  :sequence => 1
+)
+
+1.upto(45) do |i|
+  Product.create!(
+    :name => "Produto Muito Tri #{i}",
+    :application => "Este produto serve para blah blah blah.",
+    :type_id => (i%2 == 0) ? @peca.id : @maquina.id
+  )
+end
+
+1.upto(15) do |i|
+  Service.create!(
+    :title => "Serviço Bala #{i}",
+    :description => "Este serviço consiste em blah blah blah.",
+    :enabled => true
+  )
+end
