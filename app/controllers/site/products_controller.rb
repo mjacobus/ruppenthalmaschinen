98d8d18d58f1b8652bbfaa4659b4fe0b3f768c49
Marcelo Guilherme Jacobus Jr <marcelo.jacobus@gmail.com>
2012-10-02 19:44:51 -0300

    Listagem de produtos funciona tambem com filtro de tipo

--- app/controllers/site/products_controller.rb
+++ app/controllers/site/products_controller.rb
@@ -2,7 +2,14 @@ class Site::ProductsController < SiteController
   respond_to :html
   
   def index
-    @products = Product.enabled.page(params[:page]).per(params[:per])
+    @products = Product.enabled
+    
+    if params[:type].present?
+      type = ProductType.find_by_slug(params[:type])
+      @products = @products.where(:type_id => type.id)
+    end
+    
+    @products = @products.page(params[:page]).per(params[:per])
     respond_with(@products)
   end
 
--- spec/controllers/site/products_controller_spec.rb
+++ spec/controllers/site/products_controller_spec.rb
@@ -1,39 +1,12 @@
 require 'spec_helper'
 
-describe Site::ProductsController do
-  
+
+describe Site::ProductsController, "#show" do
   def show(product = Factory.create(:product), options = {})
     get :show, {:type => product.type.slug, :product => product.slug}.merge(options)
     product
   end
-
-  describe "GET 'index'" do
-    before do
-      2.times { Factory.create(:product, :enabled => true) }
-      Factory.create(:product, :enabled => false)
-    end
-    
-    it "returns http success" do
-      get 'index'
-      response.should be_success
-    end
-    
-    it "assigns enabled products to @products" do
-      get :index
-      assigns(:products).should =~ Product.enabled
-    end
-    
-    it "should paginate" do
-      q = stub("query")
-      Product.stub(:enabled).and_return(q)
-      q.should_receive(:page).with('2').and_return(q)
-      q.should_receive(:per).and_return(q)
-      get :index, :page => '2'
-    end
-    
-  end
-
-  describe "GET 'show'" do
+  
     before { @product = Factory.create(:product) }
     
     it "returns http success" do
@@ -58,6 +31,37 @@ describe Site::ProductsController do
       @product.save!
       lambda { show(@product) }.should raise_error
     end
+
+end
+
+describe Site::ProductsController, "#index" do
+  before do
+    @product = Factory.create(:product, :enabled => true)
+    Factory.create(:product, :enabled => true)
+    Factory.create(:product, :enabled => false)
+  end
+  
+  describe " " do
+    subject { response }
+    before {get :index}
+    its(:status) { should == 200 }
+    it  { assigns(:products).should =~ Product.enabled }
+  end
+  
+  describe "page 2" do
+    it "should paginate" do
+      q = stub("query")
+      Product.stub(:enabled).and_return(q)
+      q.should_receive(:page).with('2').and_return(q)
+      q.should_receive(:per).and_return(q)
+      get :index, :page => '2'
+    end
   end
 
+  describe "with type" do
+    it "should query for specify type" do
+      get :index, :type => @product.type.slug
+      assigns(:products).should eq([@product])
+    end
+  end
 end
