cd9dd6aacb3169d57ae9148670885a134d18be31
Marcelo Guilherme Jacobus Jr <marcelo.jacobus@gmail.com>
2012-10-02 19:49:43 -0300

    Melhorado o Site::ProductController#index

--- app/controllers/site/products_controller.rb
+++ app/controllers/site/products_controller.rb
@@ -1,11 +1,14 @@
 class Site::ProductsController < SiteController
   respond_to :html
   
+  # /produtos(/:page)
+  # /produtos/:type(/:page)
   def index
     @products = Product.enabled
     
     if params[:type].present?
       type = ProductType.find_by_slug(params[:type])
+      raise ("Tipo de produto não encontrado") unless type
       @products = @products.where(:type_id => type.id)
     end
     
@@ -13,6 +16,7 @@ class Site::ProductsController < SiteController
     respond_with(@products)
   end
 
+  # /produtos/:type/:product
   def show
     @product = Product.enabled.find_by_slug(params[:product])
     
