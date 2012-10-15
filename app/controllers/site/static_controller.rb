cd2027692b19e00e5ae693873ee6e309f7c69471
Marcelo Guilherme Jacobus Jr <marcelo.jacobus@gmail.com>
2012-10-15 20:55:13 -0300

    Alterado controller

--- app/controllers/site/static_controller.rb
+++ app/controllers/site/static_controller.rb
@@ -1,4 +1,4 @@
-class Site::StaticController < ApplicationController
+class Site::StaticController < SiteController
   def index
     template = Rails.root.join("app","views","site","static","#{params[:page]}.html.erb")
     if File.exists?(template)
