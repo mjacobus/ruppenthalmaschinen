e03c6a4f720aaf0c54169bcb0af55b7c9fc46324
Marcelo Guilherme Jacobus Jr <marcelo.jacobus@gmail.com>
2012-10-15 22:01:21 -0300

    Removido arquivo de configuracao do mini profiler

--- config/initializers/mini_profiler.rb
+++
@@ -1,6 +0,0 @@
-unless Rails.env.test?
-  Rack::MiniProfiler.config.skip_paths << '/admin'
-  Rack::MiniProfiler.config.use_existing_jquery = true
-  Rack::MiniProfiler.config.pre_authorize_cb = lambda {|env| Rails.env.development?}
-  Rack::MiniProfiler.config.position = 'right'
-end
