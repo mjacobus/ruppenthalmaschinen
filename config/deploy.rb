88ebc3387a859a3f7792a2fc177e5f509780099b
Marcelo Guilherme Jacobus Jr <marcelo.jacobus@gmail.com>
2012-10-15 21:48:16 -0300

    Alterado deploy e database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,35 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require "bundler/capistrano"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :application, "test.ruppenthalmaschinen.com.br"
+
+# user defined vars
+set :user, 'marceloadmin'
+set :deploy_to, "/webapps/#{application}"
+set :domain, "www.ruppenthalmaschinen.com.br"
+after 'deploy:update_code', 'deploy:symlink_db'
+
+
+default_run_options[:pty] = true
+   
+# ssh
+ssh_options[:forward_agent] = true
+#ssh_options[:verbose] = :debug
+
+# SCM
+set :repository,  "git@github.com:mjacobus/Diego.git"
+set :scm, :git
+set :git_enable_submodules,1
+set :branch, 'development'
+# set :git_shallow_clone, 1
+set :scm_verbose, true
+
+
+# roles (servers)
+role :web, domain                          # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true        # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -15,11 +37,24 @@ role :db,  "your slave db-server here"
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
+#
+# share database.yml
+#
+# after 'deploy:update_code', 'deploy:symlink_db'
+namespace :deploy do
+  desc "Symlinks the database.yml"
+  task :symlink_db, :roles => :app do
+    run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+  end
+end
+
+
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    # run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+    run "touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
