de02e4d1ed1592313138b23277a692139b3255bb
Marcelo Guilherme Jacobus Jr <marcelo.jacobus@gmail.com>
2012-10-24 10:58:17 -0200

    Compilação de css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Diego::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( *.js site/*.css active_admin.css active_admin.js)
+  config.assets.precompile += %w( *.js site.css active_admin.css active_admin.js)
   config.assets.initialize_on_precompile = false
 
   # Disable delivery errors, bad email addresses will be ignored
