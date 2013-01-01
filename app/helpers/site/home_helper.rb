dba4562abf9309b92a838b6e7b83ed08b1c5f344
Marcelo Guilherme Jacobus Jr <marcelo.jacobus@gmail.com>
2013-01-01 11:46:39 -0200

    Corrigido helper de video

--- app/helpers/site/home_helper.rb
+++ app/helpers/site/home_helper.rb
@@ -13,7 +13,7 @@ module Site::HomeHelper
       content_tag('iframe',nil,{
         :width => 420,
         :height => 315,
-        :src => "http://www.youtube.com/embed/#{video}?rel=0&controls=2&showinfo=0&theme=light&=1",
+        :src => "http://www.youtube.com/embed/#{product.video}?rel=0&controls=2&showinfo=0&theme=light&=1",
         :frameborder => 0,
         :allowfullscreen => :allowfullscreen
       })
