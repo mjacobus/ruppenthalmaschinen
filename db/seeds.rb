d0d9be5732159524c8bec765a8ec0454d3da11fb
Marcelo Guilherme Jacobus Jr <marcelo.jacobus@gmail.com>
2013-02-17 13:01:16 -0300

    Removido post-id e alterado seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,14 @@ def add_pictures(picturable)
   end
 end
 
+def loren_ipsun
+<<LOREN
+  Lorem ipsum dolor sit amet, labores nostrum mei cu, cu vis mundi iisque dolores. Imperdiet philosophia te vis, nam ad rebum regione accusamus. Omnis propriae per an, id ipsum erant his. Sea te nullam persecuti, eu sit audire scripta eruditi, cu vix quidam tincidunt definiebas.
+
+  Dolor mnesarchum ei mea, nulla denique cu qui. Possit commune definiebas ex sit, ad vim denique minimum adipiscing. Mei veritus abhorreant constituto ut, porro adipiscing nec ex. No populo praesent mea. Duo solum aliquid disputando no.
+LOREN
+end
+
 @maquina = ProductType.find_or_create_by_name_and_slug_and_sequence!(
   :name => 'Máquina',
   :slug => 'maquinas',
@@ -30,12 +38,16 @@ end
 @moveleiro = Category.find_or_create_by_name!(:name => 'Moveleiro')
 
 1.upto(45) do |i|
+  home = rand(10) == 9
   @product = Product.find_or_create_by_name_and_application_and_type_id_and_category_id_and_enabled!(
-    :name => "Produto Muito Tri #{i}",
-    :application => "Este produto serve para blah blah blah.",
+    :name => "Produto #{i}",
+    :application => loren_ipsun,
     :type_id => (i%2 == 0) ? @peca.id : @maquina.id,
     :category_id => (i%2 == 0) ? @automotivo.id : @moveleiro.id,
-    :enabled => true
+    :enabled => true,
+    :home => home,
+    :video => home ? 'YBrTccemsOc' : nil,
+    :video_description => home ? loren_ipsun : nil
   )
   
   @product.features << Feature.find_or_create_by_product_id_and_name_and_value(@product.id, "peso", "#{i} kilos")
@@ -44,9 +56,31 @@ end
 end
 
 1.upto(15) do |i|
+  home = rand(10) == 9
   add_pictures Service.find_or_create_by_title_and_description_and_enabled!(
-    :title => "Serviço Bala #{i}",
-    :description => "Este serviço consiste em blah blah blah.",
-    :enabled => true
+    :title => "Serviço #{i}",
+    :description => loren_ipsun,
+    :enabled => true,
+    :home => home,
   )
+end
+
+unless Snippet.find_by_slug('home.descricao')
+  Snippet.create!({
+    :slug => 'home.descricao',
+    :title => 'Descrição da home',
+    :content => 'A RUPPENTHAL & RUPPENTHAL é uma empresa do ramo metalúrgico que têm suas atividades direcionadas para construção mecânica de máquinas e equipamentos para a indústria. Fundada em 2011 está localizada no importante pólo industrial do Vale do Sinos em Novo Hamburgo/RS. A empresa apresenta soluções em projetos e construção de equipamentos baseado na otimização e na eficiência de funcionamento destes, bem como na automação e racionalização do processo, sempre buscando a racionalização de mão de obra, aumento de produtividade e economia de recursos.'
+  })
+end
+
+unless Snippet.find_by_slug('sobre-a-empresa.descricao')
+  Snippet.create!({
+    :slug => 'sobre-a-empresa.descricao',
+    :title => 'Descrição da home',
+    :content => "A RUPPENTHAL & RUPPENTHAL é uma empresa do ramo metalúrgico que têm suas atividades direcionadas para construção mecânica de máquinas e equipamentos para a indústria. Fundada em 2011 está localizada no importante pólo industrial do Vale do Sinos em Novo Hamburgo/RS. A empresa apresenta soluções em projetos e construção de equipamentos baseado na otimização e na eficiência de funcionamento destes, bem como na automação e racionalização do processo, sempre buscando a racionalização de mão de obra, aumento de produtividade e economia de recursos.
+
+Para a obtenção destes resultados dispomos de uma equipe de profissionais capacitados e comprometidos com seus objetivos, que além de atender, espera superar as necessidades dos desafios propostos.
+
+Contamos com o trabalho sério e inovador de toda a equipe, que crê na união de idéias para a obtenção de resultados excepcionais."
+  })
 end
