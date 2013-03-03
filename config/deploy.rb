377b86c4889a430d84835e384c13b2bef773a1b3
Marcelo Guilherme Jacobus Jr <marcelo.jacobus@gmail.com>
2013-03-02 23:56:31 -0300

    Atualizado capistrano e deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,29 +1,27 @@
 require "bundler/capistrano"
 
-set :application, "www.ruppenthalmaschinen.com.br"
+set :application, 'www.ruppenthalmaschinen.com.br' 
+set :repository,  "git@github.com:mjacobus/ruppenthalmaschinen.git"
 set :branch, 'master'
 
 # user defined vars
-set :user, 'marceloadmin'
+
+
+set :user, 'root'
 set :deploy_to, "/webapps/#{application}"
-set :domain, application
-
+set :domain, "do"
 after 'deploy:update_code', 'deploy:symlink_db','deploy:symlink_email_config'
-
+before "deploy:assets:precompile", 'deploy:symlink_db','deploy:symlink_email_config'
 
 default_run_options[:pty] = true
 
 # ssh
 ssh_options[:forward_agent] = true
-#ssh_options[:verbose] = :debug
 
 # SCM
-set :repository,  "git@github.com:mjacobus/ruppenthalmaschinen.git"
 set :scm, :git
 set :git_enable_submodules,1
-# set :git_shallow_clone, 1
 set :scm_verbose, true
-
 
 # roles (servers)
 role :web, domain                          # Your HTTP server, Apache/etc
@@ -54,18 +52,16 @@ namespace :deploy do
   end
 end
 
-
-# http://www.bencurtis.com/2011/12/skipping-asset-compilation-with-capistrano/
 # skips precompile if there is no change
-namespace :deploy do
-  namespace :assets do
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-      else
-        logger.info "Skipping asset pre-compilation because there were no asset changes"
-      end
-    end
-  end
-end
+#namespace :deploy do
+#  namespace :assets do
+#    task :precompile, :roles => :web, :except => { :no_release => true } do
+#      from = source.next_revision(current_revision)
+#      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+#        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+#      else
+#        logger.info "Skipping asset pre-compilation because there were no asset changes"
+#      end
+#    end
+#  end
+#end
